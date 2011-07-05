91693a54c92056d9372f86769a90574efa537778
Oliver Kiessler <kiessler@inceedo.com>
2011-07-05 14:02:12 +0200

    Added iPad Format

--- lib/encoding_dot_com/video_format.rb
+++ lib/encoding_dot_com/video_format.rb
@@ -1,6 +1,6 @@
 module EncodingDotCom
   class VideoFormat < Format #:nodoc:
-    ALLOWED_OUTPUT_FORMATS = %w{flv fl9 wmv 3gp mp4 m4v ipod iphone appletv psp zune mp3 wma}.freeze
+    ALLOWED_OUTPUT_FORMATS = %w{flv fl9 wmv 3gp mp4 m4v ipod iphone ipad appletv psp zune mp3 wma}.freeze
     
     allowed_attributes :output, :size, :bitrate, :framerate, :video_codec, :audio_bitrate, :audio_sample_rate, :audio_codec, :audio_channels_number, :audio_volume, :maxrate, :minrate, :bufsize, :keyframe, :start, :duration, :rc_init_occupancy, :crop_top, :crop_left, :crop_right, :crop_bottom, :logo_source, :logo_x, :logo_y, :logo_mode, :logo_threshold
     boolean_attributes :two_pass, :cbr, :deinterlacing, :add_meta, :turbo
